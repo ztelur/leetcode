51e1ff70e4bf89e12f318a5f2df7fc801b07d07f
ztelur <ztelur@gmail.com>
2015-04-24 11:21:50 +0800

    Palindrome Partitioning 1 dp 2 backtracking 3 c++要注意函数的值传递，&才是引用传递 4 substr函数的使用

---
+++ Palindrome Partitioning /solution2.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string> > partition(string s) {
+    	vector<string> pal;
+    	vector<vector<string> > res;
+    	dfs(s,res,pal);
+    	for(int i=0;i<res.size();i++) {
+    		for(int j=0;j<res[i].size();j++) {
+    			cout<<res[i][j]<<endl;
+    		}
+    	}
+    	return res;
+    }
+    void dfs(string s,vector<vector<string> > res,vector<string> pal) {
+    	cout<<"ddd   "<<s<<endl;
+    	int size=s.size();
+    	if (size==0) {
+    		res.push_back(pal);
+    		return ;
+    	}
+    	for(int i=0;i<size;i++) {
+    		if (isPalindrome(s.substr(0,i+1))) {    // if is palindrome
+    			cout <<""<<s.substr(0,i+1)<<endl;
+    			pal.push_back(s.substr(0,i+1));
+    			dfs(s.substr(i+1,size-i-1),res,pal);
+    			pal.pop_back();
+    		}
+    	}
+    	return ;
+    }
+    bool isPalindrome(string s) {
+    	if (s.size()==0) return false;
+    	int i=0;
+    	int j=s.size()-1;
+    	while(i<j) {
+    		if (s.at(i)!=s.at(j)) return false;
+    		i++;
+    		j--;
+    	}
+    	return true;
+    }
+};
+int main() {
+	string s("a");
+	Solution solution;
+	solution.partition(s);
+
+}
